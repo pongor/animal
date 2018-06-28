bc87b0d19bf47409dd906d2fa371e2f519dac1e3
pongor <yuan_gaopeng@126.com>
2018-06-28 17:03:00 +0800

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-      //  $url = new Url();
-
+        $this->app['url']->forceScheme('https');
     }
 
     /**
